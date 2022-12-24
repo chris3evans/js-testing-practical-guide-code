8f6c4d41f1d405a2fe8888e7f6f98cf637416541
chris3evans <chris3evans.ce@gmail.com>
2022-12-24 17:16:46 +0000

    Completed lesson 27

--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -1,14 +1,6 @@
 import { it, expect } from "vitest";
 import { transformToNumber } from "./numbers";
 
-// it("should throw an error if no argument is passed", () => {
-//   const testFn = () => {
-//     transformToNumber();
-//   };
-
-//   expect(testFn).toThrow();
-// });
-
 it("should return a number value if a number argument is entered", () => {
   const input = 1;
 
@@ -23,4 +15,12 @@ it("should return a number value if a string number argument is entered", () => 
   const result = transformToNumber(input);
 
   expect(result).toBe(1);
+});
+
+it("should yield NaN for non-transformable values", () => {
+  const input = "hello";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBeNaN();
 });
