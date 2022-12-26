9bdb8f31fbcef273c2381813a63c3ba2467acf2f
chris3evans <chris3evans.ce@gmail.com>
2022-12-26 10:58:34 +0000

    Completed lesson 44

--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -1,29 +1,41 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, it, expect } from "vitest";
+import { transformToNumber, cleanNumbers } from "./numbers";
 
-it("should return a number value if a number argument is entered", () => {
-  const input = 1;
+describe("transformToNumber()", () => {
+  it("should return a number value if a number argument is entered", () => {
+    const input = 1;
 
-  const result = transformToNumber(input);
+    const result = transformToNumber(input);
 
-  expect(result).toBe(1);
+    expect(result).toBe(1);
+  });
+
+  it("should return a number value if a string number argument is entered", () => {
+    const input = "1";
+
+    const result = transformToNumber(input);
+
+    expect(result).toBe(1);
+  });
+
+  it("should yield NaN for non-transformable values", () => {
+    const input = "hello";
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it("should return a number value if a string number argument is entered", () => {
-  const input = "1";
+describe("cleanNumbers()", () => {
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
 
-  const result = transformToNumber(input);
+    const cleanedNumbers = cleanNumbers(numberValues);
 
-  expect(result).toBe(1);
-});
-
-it("should yield NaN for non-transformable values", () => {
-  const input = "hello";
-  const input2 = {};
-
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
-
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
 });
