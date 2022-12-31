f28dd581536eb3495e28fffaea6575395b834667
chris3evans <chris3evans.ce@gmail.com>
2022-12-31 11:41:50 +0000

    Completed lesson 67

--- code/07-finished/src/util/io.test.js
+++ code/07-finished/src/util/io.test.js
@@ -4,11 +4,21 @@ import { promises as fs } from "fs";
 import writeData from "./io";
 
 vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 it("should execute the writeFile method", () => {
   const testData = "test";
   const testFileName = "test.txt";
 
-  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
-  expect(fs.writeFile).toBeCalled();
+  writeData(testData, testFileName);
+
+  expect(fs.writeFile).toBeCalledWith(testFileName, testData);
 });
