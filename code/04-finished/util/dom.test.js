ce68b0125c4d5cb199bc0dbc622ca71fd731b45b
chris3evans <chris3evans.ce@gmail.com>
2023-01-06 10:13:37 +0000

    Completed lesson 87

--- code/04-finished/util/dom.test.js
+++ code/04-finished/util/dom.test.js
@@ -1,4 +1,4 @@
-import { it, expect, vi } from "vitest";
+import { it, expect, vi, beforeEach } from "vitest";
 import fs from "fs";
 import path from "path";
 
@@ -10,12 +10,23 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocumentContent);
 vi.stubGlobal("document", document);
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+  document.write(htmlDocumentContent);
+});
 
 it('should add an error paragraph to the id="errors" element', () => {
   showError("Test Error");
 
+  const errorsEl = document.getElementById("errors");
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull();
+});
+
+it("should not contain an error paragraph initially", () => {
   const errorsEl = document.getElementById("errors");
   const errorParagraph = errorsEl.firstElementChild;
 
